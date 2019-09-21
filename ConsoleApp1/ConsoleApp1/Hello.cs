5fb0ac93d8b0e203374384d962a4d4f21f60822a
student <student@DESKTOP-OT59EC7>
2019-09-21 14:37:10 +0200

    commit.1

---
+++ ConsoleApp1/ConsoleApp1/Hello.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Hello
+    {
+    }
+}
+
+
+namespace Hello
+{
+    public static class Hello
+
+
+    {
+        public static void WriteHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+
+
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Hello.WriteHello();
+
+            
+        }
+    }
+}
