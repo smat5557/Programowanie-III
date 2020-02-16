cc2941f528e4c78d06c07da4dcc3544874c209e1
smat5557@gmail.com <smat5557@gmail.com>
2020-02-16 19:12:38 +0100

    zajecia 7

---
+++ ConsoleApp11/ConsoleApp11/Ink.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    public class Ink
+    {
+        public string Colour { get; }
+        public int InkLeft { get; set; }
+
+        public Ink(string colour, int inkLeft = 130)
+        {
+            Colour = colour;
+            InkLeft = inkLeft;
+        }
+        public static List<Ink> StandardInkSet()
+        {
+            Console.WriteLine("Standard ink set bought (cost: 100&)");
+            Console.WriteLine();
+            return new List<Ink> { new Ink("Black"), new Ink("Magenta"), new Ink("Cyan"), new Ink("Yellow") };
+        }
+        public static List<Ink> EconomyInkSet()
+        {
+            Console.WriteLine("Economy ink cost bougth (cost: 80&)");
+            Console.WriteLine();
+            return new List<Ink> { new Ink("Black", 120), new Ink("Magenta", 80), new Ink("Cyan", 80), new Ink("Yellow", 80) };
+        }
+    }
+
+}
