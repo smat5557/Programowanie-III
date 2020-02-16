546ffc776559fb5146d731cc0c5f9a4291f9a381
smat5557@gmail.com <smat5557@gmail.com>
2020-02-16 18:24:36 +0100

    zajecia3

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stopnie;
+            stopnie = Srednia.TakeInputNumbers();
+            float avg = Srednia.CalcAvg(stopnie);
+
+            Console.WriteLine("Srednia wynosi: {0}", avg.ToString());
+
+
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Srednia.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    class Srednia
+    {
+        static public float CalcAvg(string inputNumbers)
+        {
+            float Sum = 0;
+            string[] numbers = inputNumbers.Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var number in numbers)
+            {
+                Sum += Convert.ToSingle(number);
+            }
+            return (float)Sum / numbers.Length;
+        }
+        static public string TakeInputNumbers()
+        {
+
+            string answer = "";
+            StringBuilder result = new StringBuilder();
+            do
+            {
+                answer = Console.ReadLine();
+                result.Append(answer);
+                result.Append(";");
+
+            } while (answer != string.Empty);
+            return result.ToString();
+        }
+        static public string RoundFloat(float number)
+        {
+            return Math.Round(number, 2).ToString();
+        }
+    }
+}
+    
+
