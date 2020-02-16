25d4208942d06dd443b7fe7dc17ea8b83c061d62
smat5557@gmail.com <smat5557@gmail.com>
2020-02-16 18:02:57 +0100

    zajecia 2

---
+++ ConsoleApp6/ConsoleApp6/Funkcje.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    class Funkcje
+    {
+        public float ObliczSrednia(float suma, int iloscElementow)
+        {
+            return (float)suma / iloscElementow;
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder builder = new StringBuilder();
+            Funkcje obiekt = new Funkcje();
+            string text = "@";
+            float sum = 0;
+            float result = 0;
+            while (text != string.Empty)
+            {
+                text = Console.ReadLine();
+                builder.Append(text + "!");
+            }
+            string gradesString = builder.ToString();
+
+            string[] grades = gradesString.Split(new char[] { '!' }, StringSplitOptions.RemoveEmptyEntries);
+            float[] gradesInt = new float[grades.Length];
+            for (int i = 0; i < grades.Length; i++)
+            {
+                gradesInt[i] = Convert.ToSingle(grades[i]);
+            }
+            foreach (float grade in gradesInt)
+            {
+                sum += grade;
+            }
+            result = obiekt.ObliczSrednia(sum, gradesInt.Length);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+
+}
