36bbd73667c46097f8daec8ecc79d63331890186
smat5557@gmail.com <smat5557@gmail.com>
2020-02-16 18:45:57 +0100

    zajecia 4

---
+++ ConsoleApp9/ConsoleApp9/KlasaKopiujaca.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9
+{
+    class KlasaKopiujaca
+    {
+        public static void skopiujTablice(int[][] poszarpana, int[][] newPoszarpana, ref bool stan)
+        {
+            stan = true;
+            for (int i = 0; i < poszarpana.Length; i++)
+            {
+                newPoszarpana[i] = new int[poszarpana[i].Length];
+                for (int j = 0; j < poszarpana[i].Length; j++)
+                {
+                    newPoszarpana[i][j] = poszarpana[i][j];
+                    Console.Write(poszarpana[i][j]);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            newPoszarpana[1][0] = 0;
+        
+            for (int i = 0; i < poszarpana.Length; i++)
+            {
+                for (int j = 0; j < poszarpana[i].Length; j++)
+                {
+                    Console.Write(newPoszarpana[i][j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int[][] tablica =
+            {
+                new[]{1, 2, 3, 4},
+                new[]{1, 2},
+                new[]{1},
+                new[]{1, 2, 3, 4, 5, 6, 7},
+                
+            };
+
+                int[][] newTablica = new int[tablica.Length][];
+
+                bool state = false;
+                KlasaKopiujaca.skopiujTablice(tablica, newTablica, ref state);
+                Console.WriteLine(state);
+            }
+        }
+    }
