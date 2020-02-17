28e79d3325ae074c70e7812cfc84682821b5c9c9
smat5557@gmail.com <smat5557@gmail.com>
2020-02-17 19:41:51 +0100

    zajecia2

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static int iloscProduktow = 0;
+        static void UnikalnaNazwa(string nazwaProduktu)
+        {
+            iloscProduktow++;
+            Console.WriteLine("Produkt [" + nazwaProduktu + "]" + iloscProduktow);
+        }
+        static void Main(string[] args)
+        {
+            string escape = "exit";
+            string odpowiedz = "";
+            while (!odpowiedz.Equals(escape))
+            {
+                odpowiedz = Console.ReadLine();
+                switch (odpowiedz)
+                {
+                    case "1":
+                        UnikalnaNazwa("1");
+                        break;
+                    case "2":
+                        UnikalnaNazwa("2");
+                        break;
+                    case "3":
+                        UnikalnaNazwa("3");
+                        break;
+                }
+            }
+        }
+    }
+}
