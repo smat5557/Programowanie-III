d04740543869b0ad0262b8a586cfa477ef6b5b68
smat5557@gmail.com <smat5557@gmail.com>
2020-02-20 21:57:10 +0100

    zajecia 10

---
+++ ConsoleApp16/ConsoleApp16/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp16
+{
+    public class Person
+    {
+        public Person(int id,
+
+                         string name,
+
+                         string surname,
+
+                         string phone)
+
+        {
+
+            Id = id;
+
+            Name = name;
+
+            Surname = surname;
+
+            Phone = phone;
+
+        }
+
+       
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+
+}
